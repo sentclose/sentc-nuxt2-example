6c88bfb42a94b4363dfc625086a4941466ec8401
Jörn Heinemann <joernheinemann@gmx.de>
2022-08-23 13:38:47 +0200

    use client plugin for load user not mw

---
+++ plugins/getUser.ts
@@ -0,0 +1,35 @@
+import {Context} from "@nuxt/types";
+import {Sentc} from "@sentclose/sentc";
+
+/**
+ * It is important to init the user in a client plugin, because ssr on true can't handle import.meta.url
+ * @param store
+ */
+export default async function({store}: Context) {
+	if (!process.client) {
+		return;
+	}
+
+	const init = store.getters["user/User/isInit"];
+
+	if (init) {
+		return;
+	}
+
+	const user = await Sentc.init({
+		// @ts-ignore -> env must be set
+		app_token: process.env.NUXT_ENV_APP_PUBLIC_TOKEN,
+		base_url: process.env.NUXT_ENV_BASE_URL,
+		wasm_path: "/sentc_wasm_bg.wasm"
+	});
+
+	store.commit("user/User/setInit", true);
+
+	if (!user) {
+		store.commit("user/User/setLoginStatus", false);
+		return;
+	}
+
+	store.commit("user/User/setLoginStatus", true);
+	store.commit("user/User/setUser", user);
+}
